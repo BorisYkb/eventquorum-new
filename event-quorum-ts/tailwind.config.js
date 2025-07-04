be58006393cf5ca9f9af7bc6339c4bf5e4c5c2f1
BorisYkb <byakoue@gmail.com>
2025-07-04 23:05:34 +0000

    Sauvegarde des modifs locales avant pull

--- event-quorum-ts/tailwind.config.js
+++ event-quorum-ts/tailwind.config.js
@@ -1,11 +1,27 @@
 const config = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}', // ← ajoute cette ligne si tu utilises l'app router
+  ],
+  safelist: [
+    'bg-green-50',
+    'bg-white',
+    'bg-pink-50',
+    'bg-blue-50',
+    'text-yellow-400',
+    'text-green-400',
+    'text-red-400',
+    'text-blue-400',
+    'text-amber-700',
+    'text-amber-100',
+    'dark:bg-gray-900',
+  ],
   theme: {
     extend: {},
   },
   corePlugins: {
-    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
-    preflight: false,
+    preflight: false, // si tu veux garder CssBaseline de MUI
   },
   plugins: [],
 };
