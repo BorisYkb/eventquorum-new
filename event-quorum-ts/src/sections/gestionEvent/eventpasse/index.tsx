1bcae570deed7ddd3a8e7f3e5a6362d90439b3b7
unknown <orianenancy7@gmail.com>
2025-09-29 14:41:05 +0000

    evenement passe: fait Oriane29/09/2025

--- event-quorum-ts/src/sections/gestionEvent/eventpasse/index.tsx
+++ event-quorum-ts/src/sections/gestionEvent/eventpasse/index.tsx
@@ -1,5 +1,53 @@
+'use client';
 
+import React from 'react';
+import { useState } from 'react';
+import { useBoolean } from 'minimal-shared/hooks';
+
+import { Box } from '@mui/material';
+import { Typography } from '@mui/material';
+
+import { Upload } from 'src/components/upload';
 
 export default function EventPasse() {
-  return <div>Evènement passé Section</div>;
+
+  const [files, setFiles] = useState<(File | string)[]>([]);
+  const [showPreview, setShowPreview] = React.useState(true);
+  const handleDropMultiFile = (acceptedFiles: File[]) => {
+      setFiles([...files, ...acceptedFiles]);
+  };
+  const handleRemoveFile = (inputFile: File | string) => {
+      const filesFiltered = files.filter((fileFiltered) => fileFiltered !== inputFile);
+      setFiles(filesFiltered);
+  }
+  const handleRemoveAllFiles = () => {
+    setFiles([]);
+  };
+
+  return (
+    <Box sx={{ mb: 5, display: 'flex', gap: 1, flexDirection: 'column', boxShadow: 3, p: 3, borderRadius: 2 }}>
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: 600,
+            mb: 2,
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          Images des événements passés
+        </Typography>
+        <Upload
+          multiple
+          thumbnail={showPreview}
+          value={files}
+          onDrop={handleDropMultiFile}
+          onRemove={handleRemoveFile}
+          onRemoveAll={handleRemoveAllFiles}
+          onUpload={() => console.info('ON UPLOAD')}
+        />
+    </Box>
+
+  );
 }
+
